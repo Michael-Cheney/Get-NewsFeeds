3cbbdf49b92134a4965597ba8db8bf5af6c88e09
Michael-Cheney <31533370+Michael-Cheney@users.noreply.github.com>
2018-03-26 13:41:22 +1100

    Add files via upload

---
+++ Get-Newsfeed.ps1
@@ -0,0 +1,34 @@
+﻿function Get-Newsfeed
+{
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param
+    (
+        # Specifies the RSS feed URL
+        [Parameter(HelpMessage = 'Enter a valid RSS feed URL',
+                   Position = 0,
+                   Mandatory=$true,
+                   ValueFromPipeline=$false)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $NewsFeed,
+        # Specifies the number of articles to get
+        [Parameter(Mandatory=$false,
+                   ValueFromPipeline=$false)]
+        [int]
+        $Number = 3
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $news = Invoke-RestMethod -Uri $NewsFeed.ToString()
+        $news | Select-Object -First $Number | Select-Object -ExpandProperty title
+    }
+    End
+    {
+    # Optional One Time commands after process runs
+    }
+}
